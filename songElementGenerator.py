08961e33154dd45741ffd317a7fdf8b6896a05e7
gphill1396 <50145275+gphill1396@users.noreply.github.com>
2022-07-06 04:05:00 -0400

    ran program

--- songElementGenerator.py
+++ songElementGenerator.py
@@ -19,22 +19,26 @@ with open("lines.json", encoding='utf-8') as d3:
     data3 = json.load(d3)
     d3.close  
 
+# Randomize order data is in
 random.shuffle(data1)
 
 random.shuffle(data2)
 
 random.shuffle(data3)
 
+# Create log for order of generated part ideas
 with open('generatedParts.txt', 'w', encoding='utf-8') as f:
     for x in data1:
         f.write(str(x)+"\n")
         f.close
 
+# Create log for order of generated part tips
 with open('generatedTips.txt', 'w', encoding='utf-8') as f:
     for x in data2:
         f.write(str(x)+"\n")
         f.close
 
+# Create log for order of generated part lines
 with open('generatedLines.txt', 'w', encoding='utf-8') as f:
     for x in data3:
         f.write(str(x)+"\n")
@@ -42,31 +46,37 @@ with open('generatedLines.txt', 'w', encoding='utf-8') as f:
 
 
 
-
+# Visually indicate output starts here
 print("\n"+ "--------------------------------------------------------------------------"
 +"--------------------------------------------------------------------------"+
 "\n"+"--------------------------------------------------------------------------"
 "--------------------------------------------------------------------------"+"\n")
 
-
+# Get unique list of tips
 topics = []
-
+uniqueTips = []
 for x in data2:
     topics.append(x.get("Topic"))
     uniqueTopics = list(set(topics))
 
-random.shuffle(uniqueTopics)
-
 for x in data2:
     for y in uniqueTopics:
         if x.get("Topic") == y:
-            print(x.get("Tip"))
-            print("\n")
+            uniqueTips.append(x.get("Tip"))
             uniqueTopics.remove(y)
             break
+
+random.shuffle(uniqueTips)
+
+# Print random tips
+for x in uniqueTips:
+    print(x+"\n")
+
 print("\n")
 
+# Print random parts
 print(data1[:5])
 print("\n")
 
+# Print random lines
 print(data3[:5])
